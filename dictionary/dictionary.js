eba113a1bd462a9d9beb8eb3d7a06b8e2685fe58
htong1 <htong0001@gmail.com>
2020-09-09 03:19:56 +0000

    Gives all the definitions for a word with the part of speech and the examples.

--- dictionary/dictionary.js
+++ dictionary/dictionary.js
@@ -1,25 +1,52 @@
-function define(){
+ function define(){
+debugger;
 let word = document.getElementById("word").value;
 let out = document.getElementById("output");
 fetchDef(word, out);
 }
 
-function convert(defs, out){
+/*function convert(defs, out){
   debugger;
 out.innerHTML = defs[0].definition;
-}
+}*/
 
 function fetchDef(word, out){
-let uRl = "https://www.abbreviations.com/services/v2/defs.php?uid=8018&tokenid=Fq9JjFM6sNuGbpL9&format=json&word=" + word;
+let uRl = "https://www.abbreviations.com/services/v2/defs.php?uid=8055&tokenid=RJGXiBlcpncGFlgx&format=json&word=" + word;
 
 fetch(uRl)
 .then(response => response.json())
 
 .then(res => {
-convert(res.result, out);
+displayDef(res.result, out, word);
 })
-.catch(err => {
-        // handle errors
-    });
 }
 
+function displayDef(defs, out, word) {
+    let tblHtml = `<p>Here are the definitions, part of speech, and examples of the word: ${word}</p>
+    <ol>`;
+    for (const k of defs) {
+        tblHtml += createRowDef(k);
+    }
+    tblHtml += `</ol>`;
+    out.innerHTML = tblHtml;
+}
+
+function createRowDef(k) {
+    let row = `<li>${k.term}: (${k.partofspeech}.) ${k.definition} | Example: ${k.example}</li>`
+    return row;
+}
+
+/*function displayEx(defs, out, word) {
+    let tblHtml = `<p>The Definition of ${word} is</p>
+    <ol>`;
+    for (const k of defs) {
+        tblHtml += createRowDef(k);
+    }
+    tblHtml += `</ol>`;
+    out.innerHTML = tblHtml;
+}
+
+function createRowEx(k) {
+    let row = `<li>${k.definition}</li>`
+    return row;
+}*/

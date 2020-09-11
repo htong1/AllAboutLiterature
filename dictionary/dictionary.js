7ecc09f8aa8cc363e1847826b415eb8fe900b95c
htong1 <htong0001@gmail.com>
2020-09-11 03:25:01 +0000

    I added a thesaurus.

--- dictionary/dictionary.js
+++ dictionary/dictionary.js
@@ -5,11 +5,6 @@ let out = document.getElementById("output");
 fetchDef(word, out);
 }
 
-/*function convert(defs, out){
-  debugger;
-out.innerHTML = defs[0].definition;
-}*/
-
 function fetchDef(word, out){
 let uRl = "https://www.abbreviations.com/services/v2/defs.php?uid=8055&tokenid=RJGXiBlcpncGFlgx&format=json&word=" + word;
 
@@ -23,30 +18,21 @@ displayDef(res.result, out, word);
 
 function displayDef(defs, out, word) {
     let tblHtml = `<p>Here are the definitions, part of speech, and examples of the word: ${word}</p>
-    <ol>`;
+    <table> 
+    <th> Term </th>
+    <th> Part of Speech </th>
+    <th> Definition </th>
+    <th> Example </th>`;
     for (const k of defs) {
         tblHtml += createRowDef(k);
     }
-    tblHtml += `</ol>`;
+    tblHtml += `</table>`;
     out.innerHTML = tblHtml;
 }
 
 function createRowDef(k) {
-    let row = `<li>${k.term}: (${k.partofspeech}.) ${k.definition} | Example: ${k.example}</li>`
+    let row = `<tr>
+    <td>${k.term}</td> <td>${k.partofspeech}</td> <td>${k.definition} </td> <td>${k.example}</td>
+    </tr>`
     return row;
 }
-
-/*function displayEx(defs, out, word) {
-    let tblHtml = `<p>The Definition of ${word} is</p>
-    <ol>`;
-    for (const k of defs) {
-        tblHtml += createRowDef(k);
-    }
-    tblHtml += `</ol>`;
-    out.innerHTML = tblHtml;
-}
-
-function createRowEx(k) {
-    let row = `<li>${k.definition}</li>`
-    return row;
-}*/
--- thesaurus/thesaurus.js
+++ thesaurus/thesaurus.js
@@ -1,26 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-body {
-  background-color: lightblue;
+function getWord(){
+debugger;
+let wordS = document.getElementById("wordSyno").value;
+let outS = document.getElementById("outputSyno");
+fetchSyno(wordS, outS);
 }
 
-h1 {
-  color: white;
-  text-align: center;
+function fetchSyno(wordS, outS){
+let uRl = "https://www.abbreviations.com/services/v2/syno.php?uid=8055&tokenid=RJGXiBlcpncGFlgx&format=json&word=" + wordS;
+
+fetch(uRl)
+.then(response => response.json())
+
+.then(res => {
+displaySyno(res.result, outS, wordS);
+})
 }
 
-p {
-  font-family: verdana;
-  font-size: 20px;
+function displaySyno(synos, outS, wordS) {
+    let tbl = `<p>Here are the synonyms for: ${wordS}</p>
+    <ol>`;
+    for (const k of synos) {
+        tbl += createRowSyno(k);
+    }
+    tbl += `</ol>`;
+    outS.innerHTML = tbl;
 }
-</style>
-</head>
-<body>
 
-<h1></h1>
-<p>This is a paragraph.</p>
-
-</body>
-</html>
+function createRowSyno(k) {
+    let rowSyno = `<li>${k.synonyms}</li>`
+    return rowSyno;
+}

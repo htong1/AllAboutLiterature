d33a753e2555a73e60798b7ccc0a905483e9fe51
htong1 <htong0001@gmail.com>
2020-09-14 03:00:31 +0000

    Dictionary and thesaurus updated.

--- dictionary/dictionary.js
+++ dictionary/dictionary.js
@@ -1,5 +1,4 @@
  function define(){
-debugger;
 let word = document.getElementById("word").value;
 let out = document.getElementById("output");
 fetchDef(word, out);
@@ -17,29 +16,30 @@ displayDef(res.result, out, word);
 }
 
 function displayDef(defs, out, word) {
-    let tblHtml = `<p>Here are the definitions, part of speech, and examples of the word: ${word}</p>
-    <table> 
-    <th> Term </th>
-    <th> Part of Speech </th>
-    <th> Definition </th>
-    <th> Example </th>`;
+    let tblHtml = `<p>Here are the definitions, part of speech, and examples of the word: <strong>${word}</strong></p>
+    <ol>`;
     for (const k of defs) {
         tblHtml += createRowDef(k);
     }
-    tblHtml += `</table>`;
+    tblHtml += `</ol>`;
     out.innerHTML = tblHtml;
 }
 
 function createRowDef(k) {
-    let row = ``;
+    debugger;
+    let example = "";
+    let pos = "";
     if(typeof k.example == "object"){
-      row = `<tr>
-    <td>${k.term}</td> <td>${k.partofspeech}</td> <td>${k.definition} </td> <td> There is no example. :( </td>
-    </tr>`
+      example = "N/A";
     } else {
-       row = `<tr>
-    <td>${k.term}</td> <td>${k.partofspeech}</td> <td>${k.definition} </td> <td>${k.example}</td>
-    </tr>`
+      example = k.example;
     }
-    return row;
+
+    if(typeof k.partofspeech == "object"){
+      pos = "N/A";
+    } else {
+      pos = k.partofspeech;
+    }
+    
+    return `<li> <b>${k.term}</b>: <em>(${pos})</em> -<u>${k.definition}</u> <br> Examples: <i>${example}</i></li><br>`
 }
--- thesaurus/thesaurus.js
+++ thesaurus/thesaurus.js
@@ -2,10 +2,11 @@ function getWord(){
 debugger;
 let wordS = document.getElementById("wordSyno").value;
 let outS = document.getElementById("outputSyno");
-fetchSyno(wordS, outS);
+let outA = document.getElementById("outputAnt");
+fetchSyno(wordS, outS, outA);
 }
 
-function fetchSyno(wordS, outS){
+function fetchSyno(wordS, outS, outA){
 let uRl = "https://www.abbreviations.com/services/v2/syno.php?uid=8055&tokenid=RJGXiBlcpncGFlgx&format=json&word=" + wordS;
 
 fetch(uRl)
@@ -13,6 +14,7 @@ fetch(uRl)
 
 .then(res => {
 displaySyno(res.result, outS, wordS);
+displayAnt(res.result, outA, wordS)
 })
 }
 
@@ -26,7 +28,32 @@ function displaySyno(synos, outS, wordS) {
     outS.innerHTML = tbl;
 }
 
+function createRowAnt(k) {
+   let rowAnt = ``;
+    if(typeof k.antonyms == "object"){
+      rowAnt = `<li>There are no antonyms.</li>`
+    } else {
+      rowAnt = `<li>${k.antonyms}</li>`
+    }
+    return rowAnt;
+}
+
+function displayAnt(ants, outA, wordS) {
+    let atbl = `<p>Here are the antonyms for: ${wordS}</p>
+    <ol>`;
+    for (const k of ants) {
+        atbl += createRowAnt(k);
+    }
+    atbl += `</ol>`;
+    outA.innerHTML = atbl;
+}
+
 function createRowSyno(k) {
-    let rowSyno = `<li>${k.synonyms}</li>`
+    let rowSyno = ``;
+    if(typeof k.synonyms == "object"){
+      rowSyno = `<li>There are no synonyms.</li>`
+    } else {
+      rowSyno = `<li>${k.synonyms}</li>`
+    }
     return rowSyno;
 }
